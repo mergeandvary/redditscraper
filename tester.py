676d118f38faeac8f4343ab3ad23ca54e77c92f9
Ubuntu <ubuntu@ubuntuinstance.novalocal>
2016-05-20 00:29:32 +0000

    tester

---
+++ tester.py
@@ -0,0 +1,18 @@
+import praw
+import pprint
+import sys
+
+# Set default encoding to utf-8 so we dont get ascii encoding errors
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+SEARCH_TERMS        = ('INeedMasculism', )
+
+r = praw.Reddit(user_agent='linux:academic.research.comments.scraper.tester:v0.2.3 (by /u/mergeandvary)')
+
+for searchterm in SEARCH_TERMS:
+    print 'Searching for term: ' + str(searchterm)
+    submissions = r.search(searchterm, subreddit=None, sort=None, syntax=None, period=None)
+    for submission in submissions:
+        print '\nSUBMISSION ' + str(submission)
+        pprint.pprint(vars(submission))
